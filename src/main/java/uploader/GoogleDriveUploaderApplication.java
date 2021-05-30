5aaae115004142eedc0f7bb9e5974e2ab493d99d
Rafał Klimek <rafal1klimek@gmail.com>
2021-05-30 18:49:05 +0200

    Initial commit

---
+++ src/main/java/uploader/GoogleDriveUploaderApplication.java
@@ -0,0 +1,13 @@
+package uploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleDriveUploaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleDriveUploaderApplication.class, args);
+    }
+
+}
